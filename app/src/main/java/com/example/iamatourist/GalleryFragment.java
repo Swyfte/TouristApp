90b92b1d1be1354a17c51eacf8fe1d6ca0bdd7a1
top9 <top9@aber.ac.uk>
2019-07-02 16:47:41 +0100

    Update GalleryFragment.java

--- app/src/main/java/com/example/iamatourist/GalleryFragment.java
+++ app/src/main/java/com/example/iamatourist/GalleryFragment.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -65,7 +66,7 @@ public class GalleryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_gallery, container, false);
     }
 
