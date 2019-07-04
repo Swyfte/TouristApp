a19843b046bec711e0899a30bdf2555a2cdbf949
top9 <top9@aber.ac.uk>
2019-07-04 23:40:58 +0100

    Added persistence

--- app/src/main/java/com/example/iamatourist/SlideshowFragment.java
+++ app/src/main/java/com/example/iamatourist/SlideshowFragment.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.SearchView;
-import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,6 +57,7 @@ public class SlideshowFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
