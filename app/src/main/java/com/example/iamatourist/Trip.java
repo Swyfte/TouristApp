3ed3020b498f2aae20aaa673fa06224c2ce18c97
top9 <top9@aber.ac.uk>
2019-07-11 18:19:36 +0100

    Create Trip.java
    A class to model a trip, which is a gallery of images

---
+++ app/src/main/java/com/example/iamatourist/Trip.java
@@ -0,0 +1,44 @@
+package com.example.iamatourist;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Trip {
+    private ArrayList<Image> gallery;
+    private String title;
+    private Date date;
+
+    public Trip() {
+        this.gallery = new ArrayList<>();
+    }
+
+    public Trip(String title, Date date) {
+        this.gallery = new ArrayList<>();
+        this.title = title;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void addImage(Image img){
+        gallery.add(img);
+    }
+
+    public Image getTopImage(Integer index){
+        return gallery.get(index);
+    }
+}
