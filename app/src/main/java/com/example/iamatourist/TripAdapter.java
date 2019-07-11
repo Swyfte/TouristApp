ccb8d0ab3accd71519c359fa13bda20bf3356aab
top9 <top9@aber.ac.uk>
2019-07-11 18:20:25 +0100

    Create TripAdapter.java
    An adapter to allow the list of trips to be connected to the trip gallery

---
+++ app/src/main/java/com/example/iamatourist/TripAdapter.java
@@ -0,0 +1,24 @@
+package com.example.iamatourist;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TripAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+    @NonNull
+    @Override
+    public ImageAdapter.ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageAdapter.ImageViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
