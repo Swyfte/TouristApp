d285abeffc68d21e15e4745db944bf4ba648389e
Swyfte <arianwenpaganuzi@gmail.com>
2019-07-21 16:59:42 +0100

    Update TripAdapter.java
    Readied adapter for data binding

--- app/src/main/java/com/example/iamatourist/TripAdapter.java
+++ app/src/main/java/com/example/iamatourist/TripAdapter.java
@@ -1,24 +1,101 @@
 package com.example.iamatourist;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class TripAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+import java.util.List;
+
+public class TripAdapter extends RecyclerView.Adapter<TripAdapter.ViewHolder> {
+    private Context context;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView topImg;
+        ImageView midImg;
+        ImageView botImg;
+        TextView tripTitle;
+        ImageButton menuBtn;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            topImg = itemView.findViewById(R.id.TopImage);
+            midImg = itemView.findViewById(R.id.MidImage);
+            botImg = itemView.findViewById(R.id.BottomImage);
+            tripTitle = itemView.findViewById(R.id.tripName);
+            menuBtn = itemView.findViewById(R.id.trip_menu_button);
+        }
+    }
+
+    private List<Trip> trips;
+
     @NonNull
     @Override
-    public ImageAdapter.ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public TripAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View tripView = inflater.inflate(R.layout.trip_layout, parent, false);
+        return new ViewHolder(tripView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ImageAdapter.ImageViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        Trip trip = trips.get(position);
+
+        ImageView topPhoto = holder.topImg;
+        if (trip.getGallerySize() > 0) {
+            topPhoto.setImageBitmap(trip.getImageAtPos(0).getPhoto());
+        }
+        ImageView midPhoto = holder.midImg;
+        if (trip.getGallerySize() > 1) {
+            midPhoto.setImageBitmap(trip.getImageAtPos(1).getPhoto());
+        }
+        ImageView botPhoto = holder.botImg;
+        if (trip.getGallerySize() > 2) {
+            botPhoto.setImageBitmap(trip.getImageAtPos(2).getPhoto());
+        }
+
+        TextView title = holder.tripTitle;
+        title.setText(trip.getTitle());
+
+        ImageButton menuButton = holder.menuBtn;
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu optionMenu = new PopupMenu(context, holder.menuBtn);
+                optionMenu.inflate(R.menu.trip_options_menu);
+                optionMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.trip_details:
+                                return true;
+                            case R.id.trip_delete:
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
+                optionMenu.show();
+            }
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return trips.size();
     }
+
+
 }
