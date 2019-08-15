604a71576fa788ae718e7237ce8cbff8fbd73a6b
Swyfte <arianwenpaganuzi@gmail.com>
2019-08-15 14:42:18 +0100

    Update Trip.java
    Added defensive programming to prevent crashes

--- app/src/main/java/com/example/iamatourist/Trip.java
+++ app/src/main/java/com/example/iamatourist/Trip.java
@@ -11,10 +11,20 @@ public class Trip {
     private Date date;
     private String loc;
 
+    /**
+     * Default constructor, initialises an empty gallery
+     */
     public Trip() {
         this.gallery = new ArrayList<>();
     }
 
+    /**
+     * Constructor that creates an empty gallery and also connects the various details that are
+     * supplied
+     * @param title
+     * @param date
+     * @param loc
+     */
     public Trip(String title, Date date, String loc) {
         this.gallery = new ArrayList<>();
         this.title = title;
@@ -38,6 +48,10 @@ public class Trip {
         this.date = date;
     }
 
+    /**
+     * Ad an image to the gallery.
+     * @param img Image to add
+     */
     public void addImage(Image img){
         gallery.add(img);
     }
@@ -46,14 +60,32 @@ public class Trip {
         return gallery;
     }
 
+    /**
+     * Used to retrieve an image from the gallery. It is used in the TripAdapter to
+     * fill the three ImageViews, provided it's not more than the size of the array
+     * @param pos position in the array
+     * @return the image at the position
+     */
     public Image getImageAtPos(Integer pos) {
-        return gallery.get(pos);
+        if (pos <= gallery.size()) {
+            return gallery.get(pos);
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
     }
 
+    /**
+     * Remove an image from the gallery, assuming it's there
+     * @param img The image to remove
+     */
     public void removeImage(Image img) {
         gallery.remove(img);
     }
 
+    /**
+     * Get the size of the gallery. Used by TripAdapter to prevent out of bounds exceptions
+     * @return size of the gallery
+     */
     public Integer getGallerySize() {
         return gallery.size();
     }
